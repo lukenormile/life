d7241360156daca35ffd46aacddf4829a5d572e6
Luke Normile <luke.normile@outlook.com>
2017-03-20 15:25:52 -0600

    Implement rules and stepping function.

--- life.py
+++ life.py
@@ -2,23 +2,59 @@ import sys
 import curses
 from curses import wrapper
 
-DEAD_CELL = 'O'
-LIVE_CELL = 'X'
+# How cells are represented internally.
+DEAD = 0
+ALIVE = 1
 
-# Prepare the board to be overwritten by returning cursor
-# to the top of the board.
-def reset_board(board):
-    for row in board:
-        sys.stdout.write("\033[F")
+# How cells are formatted when printed.
+DEAD_PRINTCHAR = 'O'
+LIVE_PRINTCHAR = 'X'
+
+# Configurable constants for behavior of cells.
+MIN_TO_STAY_ALIVE = 2
+MAX_TO_STAY_ALIVE = 3
+MIN_TO_COME_ALIVE = 3
+MAX_TO_COME_ALIVE = 3
 
 def print_board(board):
     for row in board:
         for cell in row:
             if cell:
-                print(LIVE_CELL, end = ' ')
+                print(LIVE_PRINTCHAR, end = ' ')
             else:
-                print(DEAD_CELL, end = ' ')
+                print(DEAD_PRINTCHAR, end = ' ')
         print()
+
+def count_neighbors(board, x, y):
+    neighbors = board[x-1][y-1] \
+        + board[x-1][y]         \
+        + board[x-1][y+1]       \
+        + board[x][y-1]         \
+        + board[x][y+1]         \
+        + board[x+1][y-1]       \
+        + board[x+1][y]         \
+        + board[x+1][y+1]
+
+def update_cell(board, x, y):
+    neighbor_count = count_neighbors(board, x, y)
+    if board[x][y] == DEAD:
+        if neighbor_count >= MIN_TO_COME_ALIVE \
+                and neighbor_count <= MAX_TO_COME_ALIVE:
+                    return ALIVE
+        else:
+            return DEAD
+    else:
+        if neighbor_count >= MIN_TO_STAY_ALIVE \
+                and neighbor_count <= MAX_TO_STAY_ALIVE:
+                    return ALIVE
+        else:
+            return DEAD
+
+def step(board):
+    new_board = board
+    for x in range(board_x):
+        for y in range(board_y):
+            update_cell(board, x, y)
 
 # Get a board dimension value from the user.
 def get_dimension(stdscr, name="width"):

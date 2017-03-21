57a0ef43193260a40c5f0700468281e7b55464a1
Luke Normile <luke.normile@outlook.com>
2017-03-21 09:01:47 -0600

    Add functions to update all cells one step.

--- life.py
+++ life.py
@@ -10,6 +10,12 @@ ALIVE = 1
 LIVE_PRINTCHAR = 'X'
 DEAD_PRINTCHAR = 'O'
 
+# Values for determining the next state of a cell.
+MIN_STAY_ALIVE = 2
+MAX_STAY_ALIVE = 3
+MIN_COME_ALIVE = 3
+MAX_COME_ALIVE = 3
+
 # Prepare the board to be overwritten by returning cursor
 # to the top of the board.
 def reset_board(board):
@@ -51,12 +57,44 @@ def build_board(stdscr):
     board = [[0 for y in range(board_y)] for x in range(board_x)]
     return board
 
+def count_neighbors(board, x, y):
+    neighbors = board[x-1][y-1] \
+            + board[x-1][y  ]   \
+            + board[x-1][y+1]   \
+            + board[x  ][y-1]   \
+            + board[x  ][y+1]   \
+            + board[x+1][y-1]   \
+            + board[x+1][y  ]   \
+            + board[x+1][y+1]
+    return neighbors
+
+def update_cell(board, x, y):
+    board[x][y] = cell
+    neighbors = count_neighbors(board, x, y)
+    if cell == ALIVE:
+        if neighbors < STAY_ALIVE_MIN or neighbors > STAY_ALIVE_MAX:
+            return DEAD
+        else:
+            return ALIVE
+    else:
+        if neighbors < COME_ALIVE_MIN or neighbors > COME_ALIVE_MAX:
+            return DEAD
+        else:
+            return ALIVE
+
+def step(board):
+    next_board = board
+    for x_index, col in enumerate(board):
+        for y_index, cell in enumerate(col):
+            next_board[x_index][y_index] = update_cell(board, x_index, y_index)
+    return next_board
 
 def main(stdscr):
     stdscr.clear()
 
     board = build_board(stdscr)
-    update_display(stdscr, board)
+    while True:
+        update_display(stdscr, board)
 
     stdscr.refresh()
     stdscr.getkey()
